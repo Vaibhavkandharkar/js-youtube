1b1c5da520e75d16cd3a8b13ad6fc747dcc1520a
Vaibhavkandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-03-02 19:29:39 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144344
+let accountEmail = "vaibhavkandharkar8@gmail.com"
+var accountPassward = "Aurangabad"
+
+
+//accountId = 2c // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassward = "212121221"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+
+/*
+Prefer not to use var
+becaause if issue in block scope and functional scope
+
+*/
+
+console.table([accountEmail,accountId,accountPassward,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all JS code as newer version
+
+//alert("hello") // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability shiuld be high
+
+    //mdn and tc39 website for javascript
+
+console.log("Vaibhav")
+
+let name = "vaibhav"
+let age = 12
+let isLoggesIn = true
+
+// number => 2 to power 23
+// bogint
+// string
+// boolean ==> true/false
+// null ==> standalone value 
+// undefined ==> 
+// symbol ==> unique
+
+
+// object 
+
+console.log(typeof age); // object
+console.log(typeof undefined); // undefined
