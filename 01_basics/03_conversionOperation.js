46cf20869dfb9702200e38f115823432d3cab8a8
Vaibhavkandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-03-02 20:11:36 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(tyoeof(score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33 ==> 33"
+// "33abc" => NaN
+// true => 1; flase => 0
+
+
+let isLoggesIn = 1
+let booleanIsLogIn = Boolean(isLoggesIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "histesh" => true
+
+let someNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// *********************** Operation ******************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = srt1 + str2
+console.log(str3);
