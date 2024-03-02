963041a3f5e11255b7b3014607745563f02e44d2
Vaibhavkandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-03-02 19:47:39 +0000

    conversion of datatypes

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(tyoeof(score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33 ==> 33"
+// "33abc" => NaN
+// true => 1; flase => 0
+
+
+let isLoggesIn = 1
+let booleanIsLogIn = Boolean(isLoggesIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "histesh" => true
+
+let someNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
