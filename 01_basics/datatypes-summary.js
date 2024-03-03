f9f137543e63f6dad8b8f95499d00d60e5fe23de
Vaibhavkandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-03-03 08:11:24 +0000

    datatypes summery

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// # Primitive
+
+// 7 types : String,Number,Boolean,null,undefined,symbol, BigInt
+
+const score = 100
+const soreValue = 100.3
+
+const isLoggedin = false
+const outsideTemp = nulllet 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 323242343252423242324n
+
+
+
+// reference (Non primitive)
+
+//Array, objects,Functions
+
+
+const heros = ["shaktiman","naagraj","doga"]
+let myObj = {
+    name:"vaibhav",
+    age:21,
+}
+
+const myfunction = function(){
+    console.log("Hello world");
+} 
+
+console.log(typeof myFunction);
+ 
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
