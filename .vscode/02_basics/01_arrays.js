c02066a87df74a7fcb2c3139fade47bddbb155a8
Vaibhavkandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-04-11 11:57:39 +0000

    Array

--- .vscode/02_basics/01_arrays.js
+++ .vscode/02_basics/01_arrays.js
@@ -1,2 +1,23 @@
 // array
 const myArr = [0 , 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman, naagraj"]
+
+ const myArr2 = new Array(1,2,3,4)
+console.log(myArr(0))
+
+//Array methods
+
+myArr.push(0)
+myArr.push(7)
+myArr.pop()
+console.log(myArr);
+
+myArr.unshift(0)
+myArr.shift()
+console.log(myArr);
+// console.log(myArr,include(9));
+// console.log(myArr.indexOf(9));
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(typeof newArr)
