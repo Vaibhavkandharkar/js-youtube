10a000b101ee2fadc10d77e9cc8416aa8e75d767
Vaibhavkandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-04-14 12:20:48 +0000

    arrays

--- .vscode/02_basics/01_arrays.js
+++ .vscode/02_basics/01_arrays.js
@@ -21,3 +21,17 @@ console.log(myArr);
 const newArr = myArr.join()
 console.log(myArr);
 console.log(typeof newArr)
+
+
+// slice, splice
+
+console.log("A ", myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.slice(1,3)
+console.log("C ", myArr);
+console.log(myn2);
+
