52b64762437f827271661e4f461a643dae9a60e4
Vaibhavkandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-04-26 10:58:12 +0000

    scope

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,8 +1,48 @@
-// if
-const isUserloggedIn = true
-const temperature = 41 
+// // if
+// const isUserloggedIn = true
+// const temperature = 41 
 
-if ( temperature < 50){
-    console.log("less then 50");
+// if ( temperature < 50){
+//     console.log("less then 50");
+// }
+// conosle.log("temperature is greater than 50");
+
+// const score = 200
+// if(score > 100) {
+//     const power = "fly"
+//     console.log(`user power ${power}`);
+// }
+
+// short hand notation
+
+// const balance = 1000
+// if(balance > 500) console.log("test"); //....Implicet scope 
+
+// another way to write
+// const balance = 1000
+// if(balance > 500) console.log("test"),
+// conosle.log("vaibhav");
+
+// if(balance < 500){
+//     console.log("less than");
+// } else if(balance < 750){
+//     console.log("less than 750");
+// } else if(balance < 900){
+//     console.log("less than 900");
+// } else{
+//     console.log("less than 1200");
+// }
+
+const isUserloggedIn = true
+const debitCard = true 
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+const guestUser = true
+
+if(userLoggedIn && debitCard && 2==2){
+    console.log("Allow to buy course");
 }
-conosle.log("temperature is greater than 50");
+
+if(loggedInFromGoogle || loggedInFromEmail || guestUser) {
+  console.log("user logged in");
+}
