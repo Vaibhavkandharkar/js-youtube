43108ddc5fb58bf2c1cfc68dc70e862573f143ef
Vaibhavkandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-04-30 08:45:26 +0000

    basics_loops

--- 05_iterations /six.js
+++ 05_iterations /six.js
@@ -1,6 +1,11 @@
-const coding = ["js","ruby","java","python","cpp"]
+// const coding = ["js","ruby","java","python","cpp"]
 
-coding.forEach((item)=>{
-    console.log(item);
-})
-console.log(value);
+// const values = coding.forEach((item)=>{
+//     console.log(item);
+//     return item
+// })
+// console.log(values);
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+myNums.filtor
