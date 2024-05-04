33b1ee4cde84cf0b7234f25c692b6629057b5841
Vaibhavkandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-05-04 10:18:05 +0000

    basics of forEach

--- 05_iterations /six.js
+++ 05_iterations /six.js
@@ -7,5 +7,18 @@
 // console.log(values);
 
 
-const myNums = [1,2,3,4,5,6,7,8,9,10]
-myNums.filtor
+// const myNums  = [1,2,3,4,5,6,7,8,9,10]
+
+// const myNums = myNums.filter( (num) => num > 4)
+
+
+const newNums = {}
+myNums.forEach( (num) =>{
+    if (num > 4){
+        nuwNums.push(num)
+    }
+})
+
+
+ 
+console.log(newNums);
