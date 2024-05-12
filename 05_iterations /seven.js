8e6440b26fb70fe2d2b0ddf97a26d9b80cbfd8aa
Vaibhavkandharkar <123285690+Vaibhavkandharkar@users.noreply.github.com>
2024-05-12 10:20:04 +0000

    basics_javascript

---
+++ 05_iterations /seven.js
@@ -0,0 +1,17 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = myNumber.map( (num) => num + 10)
+// console.log(newNums);
+
+// while opening the scope there should be retun in the program
+
+// const newNums = myNumber.map( (nums) =>{ return num + 10})
+// console.log(newNums); //opening the scope
+
+
+
+const newNums = newNumber
+                .map( (num) => num * 10)
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
--- 05_iterations /six.js
+++ 05_iterations /six.js
@@ -9,16 +9,48 @@
 
 // const myNums  = [1,2,3,4,5,6,7,8,9,10]
 
-// const myNums = myNums.filter( (num) => num > 4)
+// const newNums = myNums.filter( (num) => num > 4)
 
 
-const newNums = {}
-myNums.forEach( (num) =>{
-    if (num > 4){
-        nuwNums.push(num)
-    }
+// const newNums = []
+// myNums.forEach( (num) =>{
+//     if (num > 4){
+//         nuwNums.push(num)
+//     }
+// })
+
+// myNums.forEach( (num2) => {
+//     newNum2
+// })
+ 
+// console.log(newNums);
+
+
+
+const books=[
+     {title: 'Book One', genre:'Friction', publish:1981, edition: 2004},
+     {title: 'Book Two', genre:'Non-Friction', publish:1992,edition: 2008},
+     {title: 'Book Three', genre:'History', publish:1999,
+     edition: 2007},
+     {title: 'Book Four', genre:'Non-Friction', publish:2009,
+     edition: 2014},
+     {title: 'Book Five', genre:'science', publish:1999,
+     edition: 2010},
+     {title: 'Book Six', genre:'Friction', publish:1987,
+     edition: 2007},
+     {title: 'Book Seven', genre:'Friction', publish:1987,
+     edition: 2007},
+]
+
+// var userBooks = books.filter( (bk) => bk.genre === 'History')
+
+// userBooks = books.filter( (bk) => {bk.publish >= 2000})
+// console.log(userBooks);
+
+userBooks = books.filter( (bk) =>{
+     return bk.publish >= 1995 && bk.genre === "History"
 })
 
+console.log(userBooks);
 
- 
-console.log(newNums);
+
